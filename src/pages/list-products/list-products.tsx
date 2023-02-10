4ed3c31a84426cac020f3e143da892cad1b4a974
Julio Soares Mendes <julio_soares_mendes@hotmail.com>
2023-02-10 03:16:46 -0300

    feat: criado scopo da tela de listagem de produtos

---
+++ src/pages/list-products/list-products.tsx
@@ -0,0 +1,11 @@
+import "./_list-products.scss";
+import { Card } from "../../shared/components/card";
+
+export const ListProducts = () => {
+   return (
+      <div className="container">
+         <header className="header"></header>
+         <Card />
+      </div>
+   );
+};
